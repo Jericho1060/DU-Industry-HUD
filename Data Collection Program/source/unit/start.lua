4211f7ae541c48e487db3c74017da683ae1147e2
Thomas <jericho1060@gmail.com>
2020-10-13 10:07:39 +0200

    data collection source code

---
+++ Data Collection Program/source/unit/start.lua
@@ -0,0 +1,65 @@
+unit.hide()
+databank = nil
+industries = {}
+for slot_name, slot in pairs(unit) do
+    if
+        type(slot) == "table"
+        and type(slot.export) == "table"
+        and slot.getElementClass
+    then
+        slot_type = slot.getElementClass():lower()
+        if slot_type == 'databankunit' then
+            databank = slot
+        end
+
+        if slot_type == "assembly line" or
+           slot_type == "glass furnace" or
+           slot_type == "3d printer" or
+           slot_type == "smelter" or
+           slot_type == "recycler" or
+           slot_type:find("refiner") or
+           slot_type:find("industry") or
+           slot_type == "transfer unit"
+        then
+            table.insert(industries, slot)
+        end
+    end
+end
+if databank ~= nil then
+    for i = 1, #industries, 1 do
+         local id = industries[i].getId()
+    	local data = {}
+         if databank.hasKey(id) == 1 then
+            data = json.decode(databank.getStringValue(id))
+            if data.command ~= nil and data.command ~= "" then
+                if data.command:lower() == "start" then
+                    industries[i].start()
+                elseif data.command:lower():find("maintain") then
+                    local splitted = strSplit(data.command, "_")
+                    local quantity = tonumber(splitted[2])
+                    industries[i].startAndMaintain(quantity)
+                elseif data.command:lower():find("batch") then
+                    local splitted = strSplit(data.command, "_")
+                    local quantity = tonumber(splitted[2])
+                    industries[i].batchStart(quantity)
+                elseif data.command:lower() == "stop" then
+                    industries[i].hardStop(0)
+                elseif data.command:lower() == "soft_stop" then
+                    industries[i].softStop()
+                end
+                data.command = ""
+            end
+         else
+            data.command = ""
+         end
+         data.id = id
+         data.status = industries[i].getStatus()
+         data.cyclesFromStart = industries[i].getCycleCountSinceStartup()
+         data.efficiency = industries[i].getEfficiency()
+         data.uptime = industries[i].getUptime()
+         local jsonData = json.encode(data)
+         databank.setStringValue(id, jsonData)
+    	--system.print(jsonData)
+    end
+end
+unit.exit()
