cba12edf6802e921efaf2844d5e20d1c546c2964
Thomas <jericho1060@gmail.com>
2022-08-06 21:06:34 +0200

    v3.0.7-alpha - improved refresh speed of the list

--- Master program/source/unit/onStart.lua
+++ Master program/source/unit/onStart.lua
@@ -2,7 +2,7 @@
     DU Industry HUD By Jericho
 ]]
 
-local version = "V 3.0.6 - alpha"
+local version = "V 3.0.7 - alpha"
 local log_split = "================================================="
 --printing version in lua chat
 system.print(log_split)local a=""local b=math.ceil((50-#version-2)/2)for c=1,b,1 do a=a..'='end;a=a.." "..version.." "for c=1,b,1 do a=a..'='end;system.print(a)system.print(log_split)
@@ -382,7 +382,6 @@ MyCoroutines = {
             else
                 break
             end
-            coroutine.yield(coroutinesTable[5])
         end
     end,
     function()
