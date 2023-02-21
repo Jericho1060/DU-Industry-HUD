5a5124c0def815d0184c46244377937b432fdf93
Thomas <jericho1060@gmail.com>
2023-02-21 19:28:50 +0100

    fix source

--- Master program/source/system/onActionStart.lua
+++
@@ -1,6 +0,0 @@
-if hud.actions.start[action] ~= nil then
-    local success, message = pcall(hud.actions.start[action])
-    if not success then
-        system.print("Action Start '" .. action .. "' Error : " .. message)
-    end
-end
--- Master program/source/system/onActionStop.lua
+++
@@ -1,6 +0,0 @@
-if hud.actions.stop[action] ~= nil then
-    local success, message = pcall(hud.actions.stop[action])
-    if not success then
-        system.print("Action Stop '" .. action .. "' Error : " .. message)
-    end
-end
